707629fa49e3e5f5dc2d40cdfa4473a2d3e6da74
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-07-31 22:40:22 -0300

    refactor(table): updated table to be scrolled

--- frontend/src/app/components/table.tsx
+++ frontend/src/app/components/table.tsx
@@ -15,34 +15,36 @@ export function Table() {
 
     return (
         <>
-            <div className="overflow-x-auto">
-                <table className="min-w-full divide-y divide-gray-200">
-                    <thead className="bg-gray-50">
-                        <tr>
-                            <th className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Ambiente</th>
-                            <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Equipamento</th>
-                            <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Solicitação</th>
-                            <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Atendimento</th>
-                            <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                        {itemsList.map((item, index) => (
-                            <tr key={index}>
-                                <td className="p-3 text-center whitespace-nowrap text-black">{item.ambiente}</td>
-                                <td className="p-3 text-center whitespace-nowrap text-black">{item.equipamento}</td>
-                                <td className="p-3 text-center whitespace-nowrap text-black">{item.solicitacao}</td>
-                                <td className="p-3 text-center whitespace-nowrap text-black">{item.atendimento}</td>
-                                <td className="p-3 text-center whitespace-nowrap text-black">
-                                    <div className="flex justify-center items-center gap-x-2">
-                                        <BiSolidEraser />
-                                        <FaRegEdit />
-                                    </div>
-                                </td>
+            <div className="overflow-x-auto rounded-xl">
+                <div className="max-h-64 overflow-y-auto">
+                    <table className="min-w-full divide-y divide-gray-200">
+                        <thead className="bg-gray-50 sticky top-0">
+                            <tr>
+                                <th className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Ambiente</th>
+                                <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Equipamento</th>
+                                <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Solicitação</th>
+                                <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Atendimento</th>
+                                <th scope="col" className="p-3 text-center text-xs text-gray-600 uppercase tracking-wider">Ações</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-200">
+                            {itemsList.map((item, index) => (
+                                <tr key={index}>
+                                    <td className="p-3 text-center whitespace-nowrap text-black">{item.ambiente}</td>
+                                    <td className="p-3 text-center whitespace-nowrap text-black">{item.equipamento}</td>
+                                    <td className="p-3 text-center whitespace-nowrap text-black">{item.solicitacao}</td>
+                                    <td className="p-3 text-center whitespace-nowrap text-black">{item.atendimento}</td>
+                                    <td className="p-3 text-center whitespace-nowrap text-black">
+                                        <div className="flex justify-center items-center gap-x-2">
+                                            <BiSolidEraser />
+                                            <FaRegEdit />
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     );
