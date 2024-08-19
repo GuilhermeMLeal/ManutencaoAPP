6bec31971932753551026f59fbd903fd744bd42c
Guilherme <guilherme.mleal.11@gmail.com>
2024-08-19 09:19:57 -0300

    refactor(machine): updated machine to be with one component

--- frontend/src/app/components/machines/mainMachine.tsx
+++ frontend/src/app/components/machines/mainMachine.tsx
@@ -1,12 +1,30 @@
 import { Button, Container, Box, Typography } from "@mui/material";
 import Title from "../title";
-import CollapsibleTable from "./tableMachine";
+import CardBox from "../cardBox";
+
+const machineData = [
+  {
+    title: "Máquina A",
+    description: "Parte Superior - Aço Inoxidável",
+    image: "/image/roboto.png",
+  },
+  {
+    title: "Máquina B",
+    description: "Parte Inferior - Plástico ABS",
+    image: "/image/roboto.png",
+  },
+  {
+    title: "Máquina C",
+    description: "Motor - Alumínio",
+    image: "/image/roboto.png",
+  },
+];
 
 export default function MainMachine() {
   return (
     <main className="flex-1 flex flex-col p-6 pt-24 bg-white/90">
-        <Title title={"Sistema de Gestão de Máquinas"} />
-      <Box sx={{ textAlign: 'center', mb: 4, paddingTop:4 }}>
+      <Title title={"Sistema de Gestão de Máquinas"} />
+      <Box sx={{ textAlign: "center", mb: 4, paddingTop: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Visualização Detalhada de Máquinas
         </Typography>
@@ -17,8 +35,23 @@ export default function MainMachine() {
         </a>
       </Box>
       <Container maxWidth="lg">
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-          <CollapsibleTable />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexWrap: "wrap",
+            gap: 2,
+            mt: 4,
+          }}
+        >
+          {machineData.map((machine, index) => (
+            <CardBox
+              key={index}
+              title={machine.title}
+              description={machine.description}
+              image={machine.image}
+            />
+          ))}
         </Box>
       </Container>
     </main>
