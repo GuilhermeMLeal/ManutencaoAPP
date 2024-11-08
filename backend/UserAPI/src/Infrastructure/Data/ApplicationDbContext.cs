382c76ddd31faf3f9680d9bade7bf786debfb187
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-11-08 16:00:06 -0300

    feat(ApplicationDbContext): added userSquad and Squad

--- backend/UserAPI/src/Infrastructure/Data/ApplicationDbContext.cs
+++ backend/UserAPI/src/Infrastructure/Data/ApplicationDbContext.cs
@@ -11,11 +11,16 @@ namespace UserAuth.Infrastructure.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Domain.Entities.Squad> Squads { get; set; }
+        public DbSet<UserSquad> UserSquads { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UserRole>()
                 .HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            modelBuilder.Entity<UserSquad>()
+                .HasKey(us => new { us.UserId, us.SquadId });
 
             modelBuilder.Entity<UserRole>()
                 .HasOne(ur => ur.User)
@@ -26,6 +31,17 @@ namespace UserAuth.Infrastructure.Data
                 .HasOne(ur => ur.Role)
                 .WithMany(r => r.UserRoles)
                 .HasForeignKey(ur => ur.RoleId);
+
+            modelBuilder.Entity<UserSquad>()
+                .HasOne(us => us.User)
+                .WithMany(u => u.UserSquads)
+                .HasForeignKey(us => us.UserId);
+
+            modelBuilder.Entity<UserSquad>()
+                .HasOne(us => us.Squad)
+                .WithMany(s => s.UserSquads)
+                .HasForeignKey(us => us.SquadId);
+
 
             base.OnModelCreating(modelBuilder);
         }
