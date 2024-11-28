1406dc3fd4f82af2250a7120c7d8feeb81cb20d8
Guilherme Martins <guilherme.mleal.11@gmail.com>
2024-11-27 21:39:45 -0300

    feat: added cud to place repository

--- backend/MachineAPI/src/Domain/Interfaces/IPlaceRepository.cs
+++ backend/MachineAPI/src/Domain/Interfaces/IPlaceRepository.cs
@@ -4,8 +4,19 @@ namespace MachineAPI.Domain.Interfaces
 {
     public interface IPlaceRepository
     {
+        // Recupera todos os lugares
         Task<IEnumerable<Place>> GetAllAsync();
+
+        // Recupera um lugar específico pelo ID
         Task<Place> GetByIdAsync(int id);
+
+        // Adiciona um novo lugar
         Task AddAsync(Place place);
+
+        // Atualiza um lugar existente
+        Task UpdateAsync(Place place);
+
+        // Remove um lugar existente
+        Task DeleteAsync(Place place);
     }
 }
