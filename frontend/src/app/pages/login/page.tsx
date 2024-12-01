80b153d7e855d548977fee8bd4b42de7efc3b2cb
Andrelbastida <andrelbastida@hotmail.com>
2024-12-01 00:37:54 -0300

    feat: add Snackbar

--- frontend/src/app/pages/login/page.tsx
+++ frontend/src/app/pages/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState } from "react";
 import {
   TextField,
@@ -9,24 +10,58 @@ import {
   Box,
   Avatar,
   Paper,
+  Snackbar,
+  Alert,
 } from "@mui/material";
+import { Navigate } from "react-router-dom";
+import { Home } from "@mui/icons-material";// Certifique-se de que este hook existe
+import TextFieldComponent from "@/app/components/TextFieldComponent";
+import ButtonComponent from "@/app/components/ButtonComponent";
+import { useAuth } from "@/AuthContext";
 
 export default function Login() {
-  // Estado para os valores dos campos
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-    remember: false,
-  });
+  //#region Definition
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+  const { login } = useAuth(); // Certifique-se de que `useAuth` fornece a função `login`
+  const [redirect, setRedirect] = useState(false);
+  const [remember, setRemember] = useState(false); // Estado para o checkbox "Manter logado"
+  const [error, setError] = useState(false); // Para exibir erro no login
 
-  // Função para lidar com as mudanças nos campos
-  const handleChange = (event) => {
-    const { name, value, type, checked } = event.target;
-    setFormData({
-      ...formData,
-      [name]: type === "checkbox" ? checked : value,
-    });
+  const handleLogin = async () => {
+    try {
+      await login(username, password);
+      setSuccess(true);
+      setTimeout(() => {
+        setRedirect(true);
+      }, 2000);
+    } catch (err) {
+      setSuccess(false);
+      setError(true);
+    }
   };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleLogin();
+    }
+  };
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRemember(event.target.checked);
+  };
+
+  //#region Redirect
+  if (redirect) {
+    return (
+      <>
+        <Navigate to="/home" />
+        <Home />
+      </>
+    );
+  }
 
   return (
     <Box
@@ -48,7 +83,7 @@ export default function Login() {
           border: "1px solid #e0e0e0",
         }}
       >
-        {/* Foto do usuário */}
+        {/* Avatar */}
         <Box display="flex" justifyContent="center" mb={3}>
           <Avatar
             alt="User Avatar"
@@ -72,67 +107,68 @@ export default function Login() {
           Login
         </Typography>
 
-        {/* Formulário */}
-        <form>
-          {/* Username */}
-          <TextField
-            id="username"
-            name="username"
-            label="Username"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            placeholder="Digite seu username"
-            value={formData.username} // Controlado pelo estado
-            onChange={handleChange} // Atualiza o estado
-            InputLabelProps={{
-              shrink: true, // Garante que o rótulo esteja na posição correta
-            }}
-          />
+        {/* Campos de Entrada */}
+        <TextFieldComponent
+          value={username}
+          placeholder="Username"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setUsername(e.target.value)
+          }
+          onKeyPress={handleKeyPress}
+        />
 
-          {/* Password */}
-          <TextField
-            id="password"
-            name="password"
-            label="Password"
-            type="password"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            placeholder="Digite sua senha"
-            value={formData.password} // Controlado pelo estado
-            onChange={handleChange} // Atualiza o estado
-            InputLabelProps={{
-              shrink: true, // Garante que o rótulo esteja na posição correta
-            }}
-          />
+        <TextFieldComponent
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setPassword(e.target.value)
+          }
+          onKeyPress={handleKeyPress}
+        />
 
-          {/* CheckBox */}
-          <FormControlLabel
-            control={
-              <Checkbox
-                name="remember"
-                checked={formData.remember} // Controla o estado do checkbox
-                onChange={handleChange} // Atualiza o estado ao clicar
-                color="primary"
-              />
-            }
-            label="Manter logado"
-            sx={{ marginTop: 1, marginBottom: 3 }}
-          />
+        {/* CheckBox */}
+        <FormControlLabel
+          control={
+            <Checkbox
+              name="remember"
+              checked={remember}
+              onChange={handleCheckboxChange}
+              color="primary"
+            />
+          }
+          label="Manter logado"
+          sx={{ marginTop: 1, marginBottom: 3 }}
+        />
 
-          {/* Botão de login */}
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            size="large"
-            href="/pages/machines"
-          >
-            Entrar
-          </Button>
-        </form>
+        {/* Botão de Login */}
+        <ButtonComponent
+          className="button"
+          onClick={handleLogin}
+          name="Login"
+        />
       </Paper>
+
+      {/* Snackbars */}
+      <Snackbar
+        open={success}
+        autoHideDuration={2000}
+        onClose={() => setSuccess(false)}
+      >
+        <Alert severity="success" sx={{ width: "100%" }}>
+          Login realizado com sucesso!
+        </Alert>
+      </Snackbar>
+
+      <Snackbar
+        open={error}
+        autoHideDuration={2000}
+        onClose={() => setError(false)}
+      >
+        <Alert severity="error" sx={{ width: "100%" }}>
+          Erro ao realizar login. Verifique suas credenciais.
+        </Alert>
+      </Snackbar>
     </Box>
   );
 }
