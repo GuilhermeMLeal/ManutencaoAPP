ce3be54d3c923ea50797221ab7e8bdd03b7633b8
Andrelbastida <andrelbastida@hotmail.com>
2024-12-01 00:34:41 -0300

    refactor: add Router and Routes components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+import { AuthProvider } from "@/AuthContext";
 import Index from "./pages/index/page";
 import Login from "./pages/login/page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "@/PrivateRoute";
 
 export default function Home() {
-  return <Login />;
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/"
+          element={<PrivateRoute component={Index}/>} />
+
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
