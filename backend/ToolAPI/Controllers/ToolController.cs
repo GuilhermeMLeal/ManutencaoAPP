5c4ecf34a2bde26895f2106baa547db8edfcd958
NEVES070305 <guilhermenevesdasilva321@gmail.com>
2024-12-04 23:56:56 -0300

    fix(VerifyToken): fixed Verifytoken and added to ToolAPI

--- backend/ToolAPI/Controllers/ToolController.cs
+++ backend/ToolAPI/Controllers/ToolController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using ToolApi.Controllers.Validation;
 using ToolAPI.Business;
-using ToolAPI.Controllers.Validation;
 using ToolAPI.Models;
 
 namespace ToolAPI.Controllers
