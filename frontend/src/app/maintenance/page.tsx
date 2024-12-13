b62eb53b32d0c0423f400f1f9013f339d7205013
Guilherme <guilherme@educabiz.com>
2024-12-13 15:04:55 -0300

    fix(redirect): corrected redirect per folder

--- frontend/src/app/maintenance/page.tsx
+++ frontend/src/app/maintenance/page.tsx
@@ -1,7 +1,7 @@
 import MainMaintenance from "@/app/components/maintenance/mainMaintenance";
 import Aside from "@/app/components/asideBar";
 
-export default function Index() {
+export default function Maintenance() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex-1 flex">
